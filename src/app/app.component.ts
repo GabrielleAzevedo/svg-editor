cdc4d27d5b87579e997323bc19b4cf01f871f6c7
GabrielleAzevedo <gabriellegranjaz@gmail.com>
2025-05-18 17:03:13 -0300

    feat(styles): add base CSS layout for all components except properties panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { CanvasComponent } from "./canvas/canvas.component";
+import { PropertiesPanelComponent } from "./properties-panel/properties-panel.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent, CanvasComponent, PropertiesPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
